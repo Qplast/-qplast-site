fc61c1f85af9175907301b53dd8963e4ee15aaf4
kiu_ms <kiu_ms@users.noreply.github.com>
2026-09-05 23:33:59 +0330

    visual: resize pars and arya gallery photos to hamta dims 768x1400, uniform ?v=3 cache-bust (main.js v20260905k)

--- js/main.js
+++ js/main.js
@@ -976,19 +976,19 @@
         { src: "../assets/hamta-gallery-media.jpg2026082220092.jpeg?v=2", caption: "Hamta" }
       ],
       pars: [
-        { src: "../assets/pars-gallery-1.jpg?v=2", caption: "Pars" },
-        { src: "../assets/pars-gallery-2.jpg", caption: "Pars" },
-        { src: "../assets/pars-gallery-3.jpg?v=2", caption: "Pars" },
-        { src: "../assets/pars-gallery-4.jpg", caption: "Pars" },
-        { src: "../assets/pars-gallery-5.jpg", caption: "Pars" },
-        { src: "../assets/pars-gallery-6.jpg", caption: "Pars" }
+        { src: "../assets/pars-gallery-1.jpg?v=3", caption: "Pars" },
+        { src: "../assets/pars-gallery-2.jpg?v=3", caption: "Pars" },
+        { src: "../assets/pars-gallery-3.jpg?v=3", caption: "Pars" },
+        { src: "../assets/pars-gallery-4.jpg?v=3", caption: "Pars" },
+        { src: "../assets/pars-gallery-5.jpg?v=3", caption: "Pars" },
+        { src: "../assets/pars-gallery-6.jpg?v=3", caption: "Pars" }
       ],
       arya: [
-        { src: "../assets/arya-gallery-1.jpg", caption: "Arya" },
-        { src: "../assets/arya-gallery-2.jpg", caption: "Arya" },
-        { src: "../assets/arya-gallery-3.jpg", caption: "Arya" },
-        { src: "../assets/arya-gallery-4.jpg", caption: "Arya" },
-        { src: "../assets/arya-gallery-5.jpg", caption: "Arya" }
+        { src: "../assets/arya-gallery-1.jpg?v=3", caption: "Arya" },
+        { src: "../assets/arya-gallery-2.jpg?v=3", caption: "Arya" },
+        { src: "../assets/arya-gallery-3.jpg?v=3", caption: "Arya" },
+        { src: "../assets/arya-gallery-4.jpg?v=3", caption: "Arya" },
+        { src: "../assets/arya-gallery-5.jpg?v=3", caption: "Arya" }
       ]
     };
 
